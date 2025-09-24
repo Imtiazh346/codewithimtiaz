f5e8690b9d0ad88b97d150737eb6d5e0fe58feba
Imtiaz Hussain <97449155+Imtiazh346@users.noreply.github.com>
2025-09-24 18:07:06 +0500

    widht

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -14,7 +14,7 @@ export default async function BlogPage() {
   const categories = await client.fetch(categoriesQuery);
 
   return (
-    <div className="max-w-7xl mx-auto px-6 py-12 grid grid-cols-1 lg:grid-cols-4 gap-10">
+    <div className="max-w-6xl mx-auto py-12 grid grid-cols-1 lg:grid-cols-4 gap-10">
       {/* Main content */}
       <section className="lg:col-span-3 space-y-10">
         <h1 className="text-4xl font-extrabold mb-6">Blog Posts</h1>
